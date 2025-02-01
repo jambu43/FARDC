d0841fe79e5293c2967ebe8ad8e89bfe2fe8db55
mechack tambwe <mesutarso@users.noreply.github.com>
2025-02-01 01:02:48 +0100

    structure pages

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 function Logo() {
     return (
-        <div>
-            <Image src="/logo.jpeg" alt="logo" width={200} height={150} />
+        <div className="">
+            <Image src="/logo.png" alt="logo" width={200} height={150} />
         </div>
     )
 }
---
+++ lib/constants.ts
@@ -0,0 +1,26 @@
+export const MOBILE_MENU = [
+  {
+    label: "Créer une campagne",
+    href: "/campagnes/create",
+  },
+  {
+    label: "Découvrir nos campagnes",
+    href: "/campagnes",
+  },
+  {
+    label: "Connexion",
+    href: "/login",
+  },
+  {
+    label: "Inscription",
+    href: "/register",
+  },
+  {
+    label: "FAQ",
+    href: "/faq",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
