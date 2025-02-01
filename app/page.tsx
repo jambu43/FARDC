9fd21be2b1f626d8c7ff276255a0e2dbc8fc450a
Joel AMBU <ambujoel@gmail.com>
2025-02-01 02:26:17 +0100

    category section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Category } from "@/components/home/category";
 import Hero from "@/components/home/hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Category />
     </>
   );
 }
